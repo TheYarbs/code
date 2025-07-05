ab152e7c7e37ac75dbc2d492a29df8314a880761
Andrew T. Yarbrough <andrew.t.yarbrough@gmail.com>
2025-07-04 22:40:20 -0400

    test

--- Notes_API_with_SSO_IaC/terraform/main.tf
+++ Notes_API_with_SSO_IaC/terraform/main.tf
@@ -11,21 +11,21 @@ resource "kubernetes_namespace" "notes" {
 resource "helm_release" "notes-api" {
   name       = "notes-api"
   repository = "https://charts.bitnami.com/bitnami"
-  chart      = "express"
+  chart      = "nginx" # Use a valid chart, e.g., nginx
   namespace  = kubernetes_namespace.notes.metadata[0].name
 
-  set {
-    name  = "image.repository"
-    value = "yourdockerhubuser/notes-api"
-  }
-
-  set {
-    name  = "image.tag"
-    value = "latest"
-  }
-
-  set {
-    name  = "service.type"
-    value = "ClusterIP"
-  }
+  set = [
+    {
+      name  = "image.repository"
+      value = "yourdockerhubuser/notes-api"
+    },
+    {
+      name  = "image.tag"
+      value = "latest"
+    },
+    {
+      name  = "service.type"
+      value = "ClusterIP"
+    }
+  ]
 }
